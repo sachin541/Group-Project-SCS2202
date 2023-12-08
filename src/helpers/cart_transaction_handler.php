86f41ac943e1a06b1cfe277aab292fdaa0a00feb
Sachin <sachinelvitigala541@gmail.com>
2023-12-09 03:34:08 +0530

    cart error handling

---
+++ src/helpers/cart_transaction_handler.php
@@ -0,0 +1,47 @@
+<?php
+
+
+require_once '../classes/database.php';
+require_once '../classes/cart.php';
+require_once '../classes/product.php';
+
+session_start();
+
+$database = new Database();
+$db = $database->getConnection();
+// Assume $db is your database connection
+$cart = new Cart($db);
+$product = new Product($db);
+
+$userId = $_POST['user_id']; // or however you retrieve the user ID
+$cartItems = $cart->getCartItemsByUserId($userId);
+
+$allItemsInStock = true;
+$outOfStockItems = [];
+
+foreach ($cartItems as $item) {
+    $productId = $item['id'];
+    $requestedQuantity = $item['quantity'];
+
+    // Check available stock
+    $productDetails = $product->getProductById($productId);
+    $availableStock = $productDetails['quantity'];
+
+    if ($requestedQuantity > $availableStock) {
+        $allItemsInStock = false;
+        $outOfStockItems[] = $item['product_name']; // Add the product name to the list of out-of-stock items
+    }
+}
+
+if (!$allItemsInStock) {
+    
+    
+    $_SESSION['out_of_stock_message'] = implode(", ", $outOfStockItems);
+    header('Location: ../pages/view_cart.php');
+
+    exit;
+} 
+else {
+    echo "worked";
+}
+?>
--- src/pages/view_cart.php
+++ src/pages/view_cart.php
@@ -1,8 +1,8 @@
 <?php
 require_once '../classes/database.php'; 
 require_once '../classes/cart.php';
-
 require_once '../components/headers/main_header.php';
+
 
 
 $database = new Database();
@@ -17,8 +17,7 @@ if (!isset($_SESSION['user_id'])) {
 
 $userId = $_SESSION['user_id'];
 $cartItems = $cart->getCartItemsByUserId($userId);
-$totalAmount = 0; //used in loop later to cal total in cart
-// Page content starts here
+$totalAmount = 0; //used in loop later to calculate total in cart
 ?>
 
 <!DOCTYPE html>
@@ -31,53 +30,69 @@ $totalAmount = 0; //used in loop later to cal total in cart
 </head>
 <body>
     <div class="cart-container">
-        <h1>Your Cart</h1>
-        <?php foreach ($cartItems as $item): ?>
-            <?php $subtotal = $item['price'] * $item['quantity'];
-                $totalAmount += $subtotal; ?>
-            <div class="cart-item">
-                <div class="details">
-                    <div><h3><?php echo htmlspecialchars($item['product_name']); ?></h3></div>
-                    <!-- <div><p><?php echo htmlspecialchars($item['product_description']); ?></p></div> -->
-                    
-                    <div><p>Price: Rs.<?php echo htmlspecialchars($item['price']); ?></p></div>
-                    <div><p>Sub Total: Rs.<?php echo htmlspecialchars($item['price']*$item['quantity']); ?></p></div>
-                    <div><p>Quantity: <?php echo htmlspecialchars($item['quantity']); ?></p></div>
-                </div>
+        <h1>My Cart</h1>
 
+        <?php if (empty($cartItems)): ?>
+            <p class="empty-cart-message">Your cart is empty.</p>
+        <?php else: ?>
+            <?php foreach ($cartItems as $item): ?>
+                <?php 
+                $subtotal = $item['price'] * $item['quantity'];
+                $totalAmount += $subtotal;
+                ?>
+                <div class="cart-item">
+                    <img src="data:image/jpeg;base64,<?php echo base64_encode($item['image1']); ?>" alt="<?php echo htmlspecialchars($item['product_name']); ?>">
+                    <div class="details">
+                        <div><h3><?php echo htmlspecialchars($item['product_name']); ?></h3></div>
+                        <div><p>Price: Rs.<?php echo htmlspecialchars($item['price']); ?></p></div>
+                        <div><p>Quantity: <?php echo htmlspecialchars($item['quantity']); ?></p></div>
+                        <div><p>Sub Total: Rs.<?php echo htmlspecialchars($item['price']*$item['quantity']); ?></p></div>
+                    </div>
                     <!-- Update QTY -->
-                <div class="quantity-controls">
-                    <form action="../helpers/cart_handler.php" method="post" id="update-form-<?php echo $item['id']; ?>">
+                    <div class="quantity-controls">
+                        <form action="../helpers/cart_handler.php" method="post" id="update-form-<?php echo $item['id']; ?>">
+                            <input type="hidden" name="product_id" value="<?php echo $item['id']; ?>">
+                            <input type="hidden" name="update_cart_qty" value="True">
+                            <button type="button" onclick="decreaseQuantity(<?php echo $item['id']; ?>)">-</button>
+                            <input type="text" name="quantity" value="<?php echo htmlspecialchars($item['quantity']); ?>">
+                            <button type="button" onclick="increaseQuantity(<?php echo $item['id']; ?>)">+</button>
+                        </form>
+                    </div>
+                    <!-- Delete button form -->
+                    <form action="../helpers/cart_handler.php" method="post">
                         <input type="hidden" name="product_id" value="<?php echo $item['id']; ?>">
-                        <input type="hidden" name="update_cart_qty" value="True">
-                        <button type="button" onclick="decreaseQuantity(<?php echo $item['id']; ?>)">-</button>
-                        <input type="text" name="quantity" value="<?php echo htmlspecialchars($item['quantity']); ?>">
-                        <button type="button" onclick="increaseQuantity(<?php echo $item['id']; ?>)">+</button>
-                        <!-- <input type="submit" value="Update"> -->
+                        <input type="hidden" name="remove_from_cart" value="True">
+                        <input type="submit" value="Remove" class="delete-button">
                     </form>
                 </div>
-                <img src="data:image/jpeg;base64,<?php echo base64_encode($item['image1']); ?>" alt="<?php echo htmlspecialchars($item['product_name']); ?>">
-                        <!-- Delete button form -->
-                <form action="../helpers/cart_handler.php" method="post">
-                    <input type="hidden" name="product_id" value="<?php echo $item['id']; ?>">
-                    <input type="hidden" name="remove_from_cart" value="True">
-                    <input type="submit" value="Remove" class="delete-button">
+            <?php endforeach; ?>
+
+            <div class="cart-total">
+                <h2>Total: Rs.<?php echo htmlspecialchars(number_format($totalAmount, 2)); ?></h2>
+
+                <?php
+                if (isset($_SESSION['out_of_stock_message'])) {
+                    $outOfStockItems = explode(", ", $_SESSION['out_of_stock_message']);
+                    echo "<div class='out-of-stock-message'>";
+                    echo "<p>The following items are out of stock or do not have enough quantity:</p>";
+                    foreach ($outOfStockItems as $item) {
+                        echo htmlspecialchars($item) . "<br>";
+                    }
+                    echo "</div>";
+                    unset($_SESSION['out_of_stock_message']);
+                }
+                ?>
+
+                <form action="../helpers/cart_transaction_handler.php" method="post">
+                    <input type="hidden" name="user_id" value="<?php echo htmlspecialchars($userId); ?>">
+                    <input type="hidden" name="total_amount" value="<?php echo htmlspecialchars($totalAmount); ?>">
+                    <input type="submit" value="Checkout" class="checkout-button">
                 </form>
-
-                <!-- Display Total Amount -->
-               
-
-                <!-- <img src="data:image/jpeg;base64,<?php echo base64_encode($item['image1']); ?>" alt="<?php echo htmlspecialchars($item['product_name']); ?>"> -->
-                <!-- You can add more elements here like remove button, etc. -->
             </div>
-        <?php endforeach; ?>
-        <div class="cart-total">
-                    <h2>Total: Rs.<?php echo htmlspecialchars(number_format($totalAmount, 2)); ?></h2>
-                </div>
+        <?php endif; ?>
     </div>
 </body>
 </html>
-
 
 
 <script>
