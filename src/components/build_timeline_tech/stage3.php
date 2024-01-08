98ecb2fa1d45838602787852e3149fdf9c30de0b
Sachin <sachinelvitigala541@gmail.com>
2024-01-08 23:52:10 +0530

    added stage2,stage3,stage4 for tech

--- src/components/build_timeline_tech/stage3.php
+++ src/components/build_timeline_tech/stage3.php
@@ -4,58 +4,54 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
     <link rel="stylesheet" type="text/css" href="../../resources/css/css_components/timeline_tech.css">
-    
-    
 </head>
 <body>
-    
-
-    
-
-
-
-
     <div>
-    <h1>Repair Request Progress</h1>
-<ul>
-    <li style="--accent-color:#080a0d">
-        <div class="date"><?php echo "✔️ Request created!"?></div>
-        <div class="title">
-          <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
-          <div style="margin-bottom: 20px;">Date : <?php echo $start_date?></div>
+        <h1>Repair Request Progress</h1>
+        <ul>
+        <li style="--accent-color:#080a0d">
+                <div class="date"><?php echo "✔️ Build request created!"?></div>
+                <div class="title">
+                    <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
+                    <div style="margin-bottom: 20px;">Date : <?php echo $added_timestamp?></div>
+                    <div style= "word-wrap: break-word;" class="descr">Comments: <?php echo htmlspecialchars($buildDescription); ?></div>
+                </div>
+            </li>
 
-          <div style="margin-top: 10px; margin-bottom: 20px;">Item name: <?php echo $item_name; ?></div>
-          
-          <div style= "word-wrap: break-word;" class="descr">Description: <?php echo htmlspecialchars($repairDescription); ?></div>
+            <li style="--accent-color:#080a0d">
+                <div class="date"><?php echo "✔️ Technician assigned!"?></div>
+                <div class="title">
+                    <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
+                    <div style="margin-bottom: 20px;">Technician Name : <?php echo $tech_name?></div>
+                    <div style="margin-bottom: 20px;">Technician Mobile : <?php echo $tech_mobile?></div>
+                    <div style="margin-bottom: 20px;">Assignment Date : <?php echo  $tech_assigned_date?></div>
+                    <div class="descr">A Technician has been assigned to the job.</div>
+                </div>
+            
+            </li>
 
-    </li>
-    <li style="--accent-color:#080a0d">
-        <div class="date"><?php echo "✔️ Technician assigned!"?></div>
-        <div style="margin-bottom: 20px;"></div>
-        <div style="margin-bottom: 20px;">Technician Name : <?php echo $tech_name?></div>
-        <div style="margin-bottom: 20px;">Technician Mobile : <?php echo $tech_mobile?></div>
-        <div style="margin-bottom: 20px;">Date : <?php echo $tech_date?></div>
-        <div class="descr">A Technician has been assinged to the Job. Please get into contact</div>
-    </li>
-    </li>
-    <li style="--accent-color:#29ab4c">
-        <div class="date"><?php echo "Repair in progress!"?></div>
-        <div style="margin-bottom: 20px;"></div>
-        <div style="margin-bottom: 20px;">Date : <?php echo $repair_start_date?></div>
-        <div class="descr">Repair is in progress.</div>
-        <div class="accept_button">
-        <form action="../helpers/repair_handler.php" method="post" class="details-form">
-                <input type="hidden" name="stage3_accept" value="true">
-                <input type="hidden" name="refnumber" value=<?php echo htmlspecialchars($ref_number); ?>>
-                <input type="submit" value="Next stage" class="details-button">
-        </form>
-        </div>
-    </li>
-  
-    
-</ul>
+            <li style="--accent-color:#29ab4c">
+                <div class="date"><?php echo "Repair in progress!"?></div>
+                <div class="title">
+                    <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
+                    <div style="margin-bottom: 20px;">Repair Start Date : <?php echo $build_start_date?></div>
+                    <div class="descr">The Build is currently in progress.</div>
+                </div>
+                <div class="accept_button">
+                    <form action="../helpers/build_progress.php" method="post" class="details-form">
+                        <input type="hidden" name="complete_build" value="true">
+                        <input type="hidden" name="refnumber" value="<?php echo htmlspecialchars($ref_number); ?>">
+                        <input type="submit" value="Mark as Completed" class="details-button">
+                    </form>
+                </div>
+            </li>
+
+
+            
+
+            <!-- Next stages here... -->
+        </ul>
     </div>
 </body>
 </html>
--- src/components/build_timeline_tech/stage4.php
+++ src/components/build_timeline_tech/stage4.php
@@ -4,70 +4,71 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
     <link rel="stylesheet" type="text/css" href="../../resources/css/css_components/timeline_tech.css">
-    
-    
 </head>
 <body>
-    
-
-   
-
-
-
-
     <div>
-    <h1>Repair Request Progress</h1>
-<ul>
-    <li style="--accent-color:#080a0d">
-        <div class="date"><?php echo "✔️ Request created!"?></div>
-        <div class="title">
-          <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
-          <div style="margin-bottom: 20px;">Date : <?php echo $start_date?></div>
+        <h1>Repair Request Progress</h1>
+        <ul>
 
-          <div style="margin-top: 10px; margin-bottom: 20px;">Item name: <?php echo $item_name; ?></div>
+            <li style="--accent-color:#080a0d">
+                <div class="date"><?php echo "✔️ Build request created!"?></div>
+                <div class="title">
+                    <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
+                    <div style="margin-bottom: 20px;">Date : <?php echo $added_timestamp?></div>
+                    <div style= "word-wrap: break-word;" class="descr">Comments: <?php echo htmlspecialchars($buildDescription); ?></div>
+                </div>
+            </li>
+
+            <li style="--accent-color:#080a0d">
+                <div class="date"><?php echo "✔️ Technician assigned!"?></div>
+                <div class="title">
+                    <div style="margin-bottom: 20px;">Technician Name : <?php echo $tech_name?></div>
+                    <div style="margin-bottom: 20px;">Technician Mobile : <?php echo $tech_mobile?></div>
+                    <div style="margin-bottom: 20px;">Assignment Date : <?php echo  $tech_assigned_date?></div>
+                    <div class="descr">A Technician has been assigned to the job.</div>
+                </div>
+            
+            </li>
+            
+            
+
+            <li style="--accent-color:#080a0d">
+                <div class="date"><?php echo "✔️ Build completed!"?></div>
+                <div class="title">
+                <div style="margin-bottom: 20px;"></div>
+                <div style="margin-bottom: 20px;">Date : <?php echo $build_completed_date?></div>
+                <div class="descr">Repair has been completed item is ready for collection.</div>
+                </div>
+            </li>
+
+
+            <li style="--accent-color:#29ab4c">
+                <div class="date"><?php echo "Payment and collection pending!"?></div>
+                <div style="margin-bottom: 20px;"></div>
+                <div class="accept_button">
+                <form action="../helpers/repair_handler.php" method="post" class="details-form">
+                        <input type="hidden" name="stage4_accept" value="true">
+                        <input type="hidden" name="refnumber" value=<?php echo htmlspecialchars($ref_number); ?>>
+                        <input type="submit" value="Complete" class="details-button">
+                </form>
+                </div>
+                <!-- <div style="margin-bottom: 20px;">Date : <?php echo $payment_done_date?></div> -->
+                <!-- <div class="descr">Payment Made! Request completed.</div> -->
+                
+            </li>
+            
+
+            
+
+            
+
           
-          <div style= "word-wrap: break-word;" class="descr">Description: <?php echo htmlspecialchars($repairDescription); ?></div>
 
-    </li>
-    <li style="--accent-color:#080a0d">
-        <div class="date"><?php echo "✔️ Technician assigned!"?></div>
-        <div style="margin-bottom: 20px;"></div>
-        <div style="margin-bottom: 20px;">Technician Name : <?php echo $tech_name?></div>
-        <div style="margin-bottom: 20px;">Technician Mobile : <?php echo $tech_mobile?></div>
-        <div style="margin-bottom: 20px;">Date : <?php echo $tech_date?></div>
-        <div class="descr">A Technician has been assinged to the Job. Please get into contact</div>
-    </li>
-    </li>
-    <li style="--accent-color:#080a0d">
-        <div class="date"><?php echo "✔️ Repair started!"?></div>
-        <div style="margin-bottom: 20px;"></div>
-        <div style="margin-bottom: 20px;">Date : <?php echo $repair_start_date?></div>
-        <div class="descr">Repair is in progress.</div>
-    </li>
-    <li style="--accent-color:#080a0d">
-        <div class="date"><?php echo "✔️ Repair completed!"?></div>
-        <div style="margin-bottom: 20px;"></div>
-        <div style="margin-bottom: 20px;">Date : <?php echo $repair_completed_date?></div>
-        <div class="descr">Repair has been completed item is ready for collection.</div>
-    </li>
-    <li style="--accent-color:#29ab4c">
-        <div class="date"><?php echo "Payment and collection pending!"?></div>
-        <div style="margin-bottom: 20px;"></div>
-        <div class="accept_button">
-        <form action="../helpers/repair_handler.php" method="post" class="details-form">
-                <input type="hidden" name="stage4_accept" value="true">
-                <input type="hidden" name="refnumber" value=<?php echo htmlspecialchars($ref_number); ?>>
-                <input type="submit" value="Complete" class="details-button">
-        </form>
-        </div>
-        <!-- <div style="margin-bottom: 20px;">Date : <?php echo $payment_done_date?></div> -->
-        <!-- <div class="descr">Payment Made! Request completed.</div> -->
-        
-    </li>
-    
-</ul>
+            
+
+            <!-- Next stages here... -->
+        </ul>
     </div>
 </body>
 </html>
