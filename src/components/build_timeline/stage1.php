80fa35da905f728054970ed444826b80b3732cc8
Sachin <sachinelvitigala541@gmail.com>
2024-01-08 23:28:43 +0530

    bug fixes

--- src/components/build_timeline/stage1.php
+++ src/components/build_timeline/stage1.php
@@ -4,33 +4,27 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="../../resources/css/css_components/timeline.css">
+    
+    <link rel="stylesheet" type="text/css" href="../../resources/css/css_components/timeline_tech.css">
 </head>
 <body>
     <div>
         <h1>Build Request Progress</h1>
         <ul>
             <li style="--accent-color:#080a0d">
-                <div class="date"><?php echo "✔️ Request created!"?></div>
+                <div class="date"><?php echo "✔️ Build request created!"?></div>
                 <div class="title">
                     <div style="margin-bottom: 20px;">REF No : <?php echo $ref_number?></div>
-                    <div style="margin-bottom: 20px;">Date : <?php echo $start_date?></div>
-                    <div style="margin-top: 10px; margin-bottom: 20px;">Item name: <?php echo $item_name; ?></div>
-                    <div style= "word-wrap: break-word;" class="descr">Description: <?php echo htmlspecialchars($repairDescription); ?></div>
+                    <div style="margin-bottom: 20px;">Date : <?php echo $added_timestamp?></div>
+                    <div style= "word-wrap: break-word;" class="descr">Comments: <?php echo htmlspecialchars($buildDescription); ?></div>
                 </div>
             </li>
-            <li style="--accent-color:#29ab4c">
-                <div class="date">
-                    <?php 
-                        if ($tech_assigned_date) {
-                            echo "✔️ Technician assigned!";
-                        } else {
-                            echo "Technician will be assigned shortly!";
-                        }
-                    ?>
-                </div>
+
+            <li style="--accent-color:#080a0d">
+                <div class="date"><?php echo "Technician will be assigned shortly!"?> </div>
+                
             </li>
-            <!-- Add more list items based on the repair status. Adjust the visibility and content according to what the customer should see. -->
+            <!-- Additional stages will be added here based on the build process -->
         </ul>
     </div>
 </body>
--- src/views_customer/build_details.php
+++ src/views_customer/build_details.php
@@ -6,70 +6,67 @@ require_once '../classes/build.php';
 require_once '../classes/technician.php';
 
 $database = new Database();
-
 $db = $database->getConnection();
-
 $build = new Build($db);
-
 $techobj = new Technician();
 
+echo $_POST["build_id"];
+
 if (isset($_POST["build_id"])) {
-    
-    $buildDetails = $build->getAllFromBuildById($_POST["build_id"]);
+    if (isset($_SESSION['current_build_tech'])) {
+        $buildID = $_SESSION['current_build_tech']; 
+        unset($_SESSION['current_build_tech']);
+    } else {
+        $buildID = $_POST["build_id"]; 
+    }
+
+    $buildDetails = $build->getAllFromBuildById($buildID);
 
     if ($buildDetails) {
-        //
+        // Extract build details
         $ref_number = $buildDetails['build_id'];
-
-        $customerId = $buildDetails['customer_id']; //not used 
-        $customerName = $buildDetails['customer_name']; //not used 
-
+        $customerId = $buildDetails['customer_id'];
+        $customerName = $buildDetails['customer_name'];
         $customerContact = $buildDetails['contact'];
-        //stage 1 
-        $start_date = $buildDetails['added_timestamp'];
-        //stage 2 
-        $tech_date = $buildDetails['technician_assigned_date'];
-        //stage 3 
+        $added_timestamp = $buildDetails['added_timestamp'];
+        $tech_assigned_date = $buildDetails['technician_assigned_date'];
         $build_start_date = $buildDetails['build_start_date'];
-        //stage 4 
         $build_completed_date = $buildDetails['build_completed_date'];
-        //stage 5
-        $payment_done_date = $buildDetails['build_collected_date'];
-
-        $item_name = null; 
+        $build_collected_date = $buildDetails['build_collected_date'];
+        $components_list_id = $buildDetails['components_list_id'];
+        // comments 
+        $buildDescription = $buildDetails['comments']; 
         
-
-        $repairDescription = $buildDetails['comments'];
-        
-
         $amount = $buildDetails['amount'];
         $technicianId = $buildDetails['technician_id'];
-        
+
+        // Technician details
+        if ($technicianId) {
+            $technicianDetails = $techobj->getTechnicianByStaffId($technicianId); 
+            $tech_name = $technicianDetails['staff_name'];
+            $tech_mobile = $technicianDetails['mobile_no'];
+        }
+
+        // Render build details
+        // TODO: Add HTML and PHP code to display the build details
+
     } else {
-        
         echo "No details found for the given build ID.";
     }
 } else {
-    
-    echo "build ID is not provided.";
-}
-
-if($tech_date){
-    $technicianDetails = $techobj->getTechnicianByStaffId($technicianId); 
-    $tech_mobile = $technicianDetails['mobile_no'];
-    $tech_name = $technicianDetails['staff_name'];
+    echo "Build ID is not provided.";
 }
 
 // echo $addedTimestamp; 
 
-if($payment_done_date){
+if($build_collected_date){
     require_once '../components/build_timeline/stage5.php';
 }else if($build_completed_date){
     require_once '../components/build_timeline/stage4.php';
 }else if($build_start_date){
     require_once '../components/build_timeline/stage3.php';
 
-}else if($tech_date){
+}else if($tech_assigned_date){
     require_once '../components/build_timeline/stage2.php';
 }else{
     require_once '../components/build_timeline/stage1.php';
