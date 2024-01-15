cfcb6133daca45ebf05a2535e8cbcf8f0a1776b4
AkilaBuwaneka <136682897+AkilaBuwaneka@users.noreply.github.com>
2024-01-16 02:18:08 +0530

    Add files via upload
    added

--- src/views_main/home.php
+++ src/views_main/home.php
@@ -14,21 +14,21 @@
 
 
 
-<?php require_once '../components/headers/main_header.php';?>
-    
-<div class="main-container"> 
+    <?php require_once '../components/headers/main_header.php'; ?>
 
-       
+    <div class="main-container">
+
+
 
     </div>
-    <?php include '../components/carousel.php';?>
+    <?php include '../components/carousel.php'; ?>
     <div class="secondary-container">
         <div class="arrivals">
             <!-- <?php include '../components/new_arivals_slider.php' ?> -->
         </div>
 
 
-        <div class="treasury-inner">
+        <!--<div class="treasury-inner">
             <div class="grid2-item-1">
                 <div class="grid2-inner-item-1">
                     <h3>News</h3>
@@ -40,39 +40,109 @@
                         <p>In case your requirements supersedes what the local market has to offer, we will provide you with assistance to meet these requirements.</p>
                         <p>To further facilitate your access to your needs, we offer to deliver to meet your requirements straight to where you live within Sri Lankan borders.</p>
                     </div>
-                </div>
-                <!-- <div class="grid2-inner-item-2">
+                </div>-->
+        <!-- <div class="grid2-inner-item-2">
                     <div class='slider-img-container'>
                             <img src='../../resources/images/map.png' alt='item img' class='img' />
                         </div>
                 </div> -->
+        <!--</div>
+    <div class="grid2-item-2">
+        <h3>Trending Builds</h3>
+        <div class="trending-builds-container">
+            <div class='items-trending'>
+                <div class='slider-img-container'>
+                    <img src='../../resources/images/trending_builds/image1.png' alt='item img' class='img' />
+                </div>
+                <div class='info-container'>
+                    <p>Nano-X Creator PRO V2 (AMD RYZEN™ 7000 SERIES)</p>
+                    <p>Rs. 1,130,000</p>
+                </div>
             </div>
-            <div class="grid2-item-2">
-                <h3>Trending Builds</h3>
-                <div class="trending-builds-container">
-                    <div class='items-trending'>
-                        <div class='slider-img-container'>
-                            <img src='../../resources/images/trending_builds/image1.png' alt='item img' class='img' />
-                        </div>
-                        <div class='info-container'>
-                            <p>Nano-X Creator PRO V2 (AMD RYZEN™ 7000 SERIES)</p>
-                            <p>Rs. 1,130,000</p>
-                        </div>
-                    </div>
-                    <div class='items-trending'>
-                        <div class='slider-img-container'>
-                            <img src='../../resources/images/trending_builds/image2.png' alt='item img' class='img' />
-                        </div>
-                        <div class='info-container'>
-                            <p>Nano-X Creator Lite (12th gen & DDR5)</p>
-                            <p>Rs. 505,500</p>
-                        </div>
-                    </div>
+            <div class='items-trending'>
+                <div class='slider-img-container'>
+                    <img src='../../resources/images/trending_builds/image2.png' alt='item img' class='img' />
+                </div>
+                <div class='info-container'>
+                    <p>Nano-X Creator Lite (12th gen & DDR5)</p>
+                    <p>Rs. 505,500</p>
                 </div>
             </div>
         </div>
     </div>
-    <!-- <?php include '../components/footer.php' ?> -->
+    </div>
+    </div>-->
+
+        <div class="con-1">
+            <img src="../../resources/images/main_home/pnew.png" alt="">
+            <div class="con-1-sub">
+                <p><span class="con-1-span">Discover a world of cutting-edge technology and high-quality computer
+                        products right at your
+                        fingertips.</span> Whether you're a professional looking for powerful workstations or a gaming
+                    enthusiast
+                    seeking top-notch components, we've got you covered.</p>
+                <button>Buy Now</button>
+            </div>
+        </div>
+        <div class="con-2">
+            <div class="subcon">
+                <img src="../../resources/images/main_home/con-3-img1.jpeg" alt="Avatar" class="image">
+                <button class="para">Build Your Dream PC</button>
+            </div>
+            <div class="subcon">
+                <img src="../../resources/images/main_home/con-3-img2.jpg" alt="Avatar" class="image">
+                <button class="para">Buy New Laptop</button>
+            </div>
+            <div class="subcon">
+                <img src="../../resources/images/main_home/con-3-img3.jpg" alt="Avatar" class="image">
+                <button class="para">Collect Points</button>
+            </div>
+            <div class="subcon">
+                <img src="../../resources/images/main_home/con-3-img4.webp" alt="Avatar" class="image">
+                <button class="para">Delivery Your Order</button>
+            </div>
+        </div>
+        <div class="con-3">
+            <p class="main-p">New arrivals</p>
+            <div class="products">
+                <div class="product-con">
+                    <h2>Product Name</h2>
+                    <img src="../../resources/images/products/p1.png">
+                    <p>Description of the product.</p>
+                    <span>Price: $19.99</span>
+                    <button>Add to Cart</button>
+                </div>
+                <div class="product-con">
+                    <h2>Product Name</h2>
+                    <img src="../../resources/images/products/p1.png">
+                    <p>Description of the product.</p>
+                    <span>Price: $19.99</span>
+                    <button>Add to Cart</button>
+                </div>
+                <div class="product-con">
+                    <h2>Product Name</h2>
+                    <img src="../../resources/images/products/p1.png">
+                    <p>Description of the product.</p>
+                    <span>Price: $19.99</span>
+                    <button>Add to Cart</button>
+                </div>
+                <div class="product-con">
+                    <h2>Product Name</h2>
+                    <img src="../../resources/images/products/p1.png">
+                    <p>Description of the product.</p>
+                    <span>Price: $19.99</span>
+                    <button>Add to Cart</button>
+                </div>
+                <div class="product-con">
+                    <h2>Product Name</h2>
+                    <img src="../../resources/images/products/p1.png">
+                    <p>Description of the product.</p>
+                    <span>Price: $19.99</span>
+                    <button>Add to Cart</button>
+                </div>
+            </div>
+        </div>
+        <!-- <?php include '../components/footer.php' ?> -->
 
 </body>
 
