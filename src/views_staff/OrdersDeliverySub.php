a5f25c6850c5c643e11dad2500aed8f077e5a004
Sachin <sachinelvitigala541@gmail.com>
2024-02-20 01:07:30 +0530

    bug fixes

--- src/views_staff/OrdersDeliverySub.php
+++ src/views_staff/OrdersDeliverySub.php
@@ -167,22 +167,5 @@ $pageType = "Delivery";
 
 
 <script>
-    // Function to change page based on toggle state
-    function changePage() {
-        window.location.href = document.getElementById('pageToggle').checked ? 'OrdersDeliverySub.php' : 'OrdersRetailSub.php';
-    }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        // Set the toggle state based on local storage value
-        var savedState = localStorage.getItem('toggleState') === 'true';
-        document.getElementById('pageToggle').checked = savedState;
-
-        // Add event listener to the toggle
-        document.getElementById('pageToggle').addEventListener('change', function() {
-            // Save the new state to local storage
-            localStorage.setItem('toggleState', this.checked);
-            // Change page
-            changePage();
-        });
-    });
 </script>
